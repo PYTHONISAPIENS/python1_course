feb6e0217bf3ea8815bb2342c8695d6ecf4a02bb
Catalina Perez <catalinaperezbustos@gmail.com>
2024-04-18 15:44:02 +0200

    hoy

--- variables.py
+++ variables.py
@@ -1,5 +1,21 @@
+'''
 name = "catalina"
 
 print(name)
 tu_nombre = input("escribe tu nombre ")
 print(tu_nombre)
+
+
+print(1+1)
+print(1-1)
+print(1*1)
+print(1/1)
+print(1%1)
+print(21%2)
+print(21//2)
+'''
+
+print(2>6)
+print(4<10)
+
+print("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum" =="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum")
